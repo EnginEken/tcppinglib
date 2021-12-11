e2f6662b0d4ef486e748d3d7f6197de0e2506c6c
Engin Eken <engineken@192.168.1.89>
2021-12-11 19:32:48 +0300

    Examples added

--- examples/async_multi_tcpping.py
+++ examples/async_multi_tcpping.py
@@ -8,3 +8,25 @@
 
     Examples for async_multi_tcpping
 """
+from tcppinglib import async_multi_tcpping
+import asyncio
+
+urls = [
+    # FQDNs
+    'https://www.google.com/',
+    'https://www.hepsiburada.com/', 
+    'cnn.com', 
+
+    # IP Addresses
+    '1.1.1.1'
+    ]
+
+hosts = asyncio.run(async_multi_tcpping(urls, count=5, interval=1.5))
+
+for host in hosts:
+    print(host.is_alive, host.avg_rtt_all)
+
+# True 226.72
+# True 62.649
+# True 93.685
+# True 9.165
--- examples/async_tcpping.py
+++ examples/async_tcpping.py
@@ -8,3 +8,13 @@
 
     Examples for async_tcpping
 """
+
+from tcppinglib import async_tcpping
+import asyncio
+
+async def host_specs(address):
+    host = await async_tcpping(address, count=5, interval=1.5)
+    print(host.avg_rtt_all, host.is_alive)
+
+asyncio.run(host_specs('https://www.google.com/'))
+# 215.261 True
